971d4fb36805f9ffbcc58b5516a13c2d71261b19
Alexandre <alex_roa@aol.com>
2020-08-03 17:59:33 -0300

    fix css on responsive

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -31,7 +31,7 @@ function Landing() {
             </Link>
             <Link to='/give-classes' className='give-classes'>
                 <img src={giveClassesIcon} alt='Dar aula' />
-                Dar aula
+                Dar Aula
             </Link>
         </div>
 
