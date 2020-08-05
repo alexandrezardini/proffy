2033946988ca9d9f110d6ec31af3e758cd125191
Alexandre <alex_roa@aol.com>
2020-08-05 19:00:46 -0300

    FrontEnd 1.0

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -15,8 +15,8 @@ const Select: React.FC<SelectProps> = ({label, name, options, ...rest}) => {
   return (
     <div className='select-block'>
       <label htmlFor={name}>{label}</label>
-      <select value ='' id={name} {...rest}>
-        <option value='' disabled hidden></option>
+      <select value='' id={name} {...rest}>
+        <option value='' disabled hidden>Selecione uma Opção</option>
         {options.map(option => {
           return <option key={option.value} value={option.value}>{option.label}</option>
         })}
--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,43 +1,57 @@
 import React from 'react';
 
-import whatsAppIcon from '../../assets/images/icons/whatsapp.svg'
+import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
-import './styles.css'
+import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsApp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
   return (
     <article className='teacher-item'>
       <header>
-        <img
-          src='https://avatars3.githubusercontent.com/u/54390818?s=460&u=8683e57370c7599860e9e62c79890ba0096b722e&v=4'
-          alt='Avatar'
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Alexandre Roa</strong>
-          <span>Geografia</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em
-        qualquer lugar. Tenho memória fotográfica e nunca fico perdido.
-        <br />
-        <br />
-        Ensino a galera a não se perder na vida, com lições geográficas simples
-        para você nunca mais precisar de um mapa na sua bela vida;
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$ 350,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type='button'>
-        <img src={whatsAppIcon} alt='Entrar em contato' />
+        <a
+          target='_blank'
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsApp}`}
+        >
+          <img src={whatsAppIcon} alt='Entrar em contato' />
           Entrar em Contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
-export default TeacherItem
+export default TeacherItem;
